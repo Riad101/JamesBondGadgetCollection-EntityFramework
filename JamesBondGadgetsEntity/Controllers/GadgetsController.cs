6cdf3bac59375240855ad1fac2a9842c486a1641
smriad <skmdriad@gmail.com>
2021-04-14 16:40:45 +0800

    first commit

---
+++ JamesBondGadgetsEntity/Controllers/GadgetsController.cs
@@ -0,0 +1,84 @@
+﻿
+using JamesBondGadgetsEntity.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JamesBondGadgetsEntity.Controllers
+{
+    public class GadgetsController : Controller
+    {
+
+        private ApplicationDbContext context;
+
+        public GadgetsController()
+        {
+            context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            context.Dispose();
+        }
+        // GET: Gadgets
+        public ActionResult Index()
+        {
+            List<GadgetModel> gadgets = context.Gadgets.ToList();
+            
+            return View("Index", gadgets);
+        }
+
+        public ActionResult Details(int id)
+        {
+
+            GadgetModel gadget = context.Gadgets.SingleOrDefault(g => g.Id == id);
+            return View("Details", gadget);
+        }
+
+        public ActionResult Create()
+        {
+            return View("GadgetForm");
+        }
+
+        public ActionResult Edit(int id)
+        {
+           
+
+            return View("GadgetForm");
+        }
+
+        public ActionResult Delete(int id)
+        {
+
+            GadgetModel gadget = context.Gadgets.SingleOrDefault(g => g.Id == id);
+            context.Entry(gadget).State = System.Data.Entity.EntityState.Deleted;
+            context.SaveChanges();
+
+            return Redirect("/Gadgets");
+        }       
+
+        public ActionResult processCreate(GadgetModel gadgetModel)
+        {
+            //save to the database
+
+            
+
+            return View("Details");
+        }
+
+
+        public ActionResult SearchForm()
+        {
+            return View("SearchForm");
+        }
+
+        public ActionResult SearchForName(string searchPhrase)
+        {
+            var gadgets = from g in context.Gadgets where g.Name.Contains(searchPhrase) select g;
+
+            return View("Index", gadgets);
+        }
+    }
+}
---
+++ JamesBondGadgetsEntity/Models/GadgetModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace JamesBondGadgetsEntity.Models
+{
+    public class GadgetModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        [DisplayName("Appears in this Movie")]
+        public string AppearsIn { get; set; }
+        [Required]
+        [DisplayName("Actor's Name")]
+        public string WithThisActor { get; set; }
+
+        public GadgetModel()
+        {
+            Id = -1;
+            Name = "Nothing";
+            Description = "Nothing Yet";
+            AppearsIn = "Nowhere";
+            WithThisActor = "With No one";
+        }
+
+        public GadgetModel(int id, string name, string description, string appearsIn, string withThisActor)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            AppearsIn = appearsIn;
+            WithThisActor = withThisActor;
+        }
+    }
+}
